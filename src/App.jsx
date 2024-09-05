4e5fdb4f490f029538cf5e0a9c07c418653fe288
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-09-05 13:00:16 +0500

    connect with node js and add the authantication

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 // font-awesome  for icons
 import "font-awesome/css/font-awesome.css"
 // sociket io client for client side connection
-import { io } from "socket.io-client"
 import Main from './components/Main'
 
+import { io } from "socket.io-client"
 
+
+const socket = io("http://localhost:5000")
 function App() {
 
   return (
     <>
-      <Main />
+      <Main socket={socket} />
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,56 @@
-import {React,useState} from 'react'
+import { React, useState,useEffect } from 'react'
 
 import Login from './Login'
 import Chat from './Chat'
 
-const Main = () => {
+const Main = ({socket}) => {
+
+        useEffect(() => {
+          socket.on("session",({userId,username})=>{
+                setUser(username);
+          })
+        
+          
+        }, [socket])
+        
+
+
+
     const [newUser, setnewUser] = useState("")
 
-  const [user, setUser] = useState("")
+    const [user, setUser] = useState("")
 
-  const [message, setMessage] = useState("")
+    const [message, setMessage] = useState("")
 
-  const handleChange = (e) => {
-    setnewUser(e.target.value);
+    const handleChange = (e) => {
+        setnewUser(e.target.value);
 
-    console.log(e);
-  }
+        console.log(e);
+    }
 
-  const lognewUser = () => {
-    setUser(newUser);
-    console.log("new user nameis : " + newUser);
-    // setnewUser("")
-  }
+    const lognewUser = () => {
+        // setUser(newUser);
+        console.log("new user nameis : " + newUser);
+        socket.auth={username:newUser};
+        socket.connect();
+        // setnewUser("")
+    }
 
-  return (
-    <main className='content '>
-        <div className='constainer mt-3 '>
-          {
-            user &&
-            <Chat user={user} setMessage={setMessage} message={message} />
-          }
+    return (
+        <main className='content '>
+            <div className='constainer mt-3 '>
+                {
+                    user &&
+                    <Chat user={user} setMessage={setMessage} message={message} />
+                }
 
-          {
-            !user &&
-            <Login newUser={newUser} handleChange={handleChange} lognewUser={lognewUser} />
-          }
-        </div>
-      </main>
-  )
+                {
+                    !user &&
+                    <Login newUser={newUser} handleChange={handleChange} lognewUser={lognewUser} />
+                }
+            </div>
+        </main>
+    )
 }
 
 export default Main
